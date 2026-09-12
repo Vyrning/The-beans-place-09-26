3f5a8f25d23f8f565a51f05733e46ecbc355fab5
Sean Boudreaux <esbee432@gmail.com>
2026-09-11 20:29:35 -0500

    Navbar1

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@
 /* --- YOUR IMPORTS GO HERE --- */
 import HeroSection from "./components/HeroSection";
 import FooterSection from "./components/FooterSection";
+import NavBar from "./components/NavBar";
 
 // STEP 2: Create and export the App component
 // Use: export default function App() { ... }
@@ -61,7 +62,8 @@ export default function App(){
     return (
         <div className = "app">
             {/* NavBar */}
-
+                <NavBar/>
+              
 
             {/* Hero */}
             <section className="hero bg-hero">
