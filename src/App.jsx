f53222d74723a140e0f06becf2760fa8e627e799
Sean Boudreaux <esbee432@gmail.com>
2026-09-19 16:08:38 -0500

    ProductShowcase

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ import FooterSection from "./components/FooterSection";
 import NavBar from "./components/NavBar";
 import RibbonTicker from "./components/RibbonTicker";
 import FeaturesSection from "./components/FeaturesSection";
+import ProductShowcase from "./components/ProductShowcase";
 
 // STEP 2: Create and export the App component
 // Use: export default function App() { ... }
@@ -85,7 +86,7 @@ export default function App(){
             
             {/* Product showcase */}
             <section className="bg-cta">
-                
+                <ProductShowcase/>
             </section>
 
             <RibbonTicker/>
